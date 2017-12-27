2057b942c80acfa7c36ba24ace5225f826f2c1c3
Michael Higgins <mhiggins13@georgefox.edu>
2017-12-26 21:11:25 -0800

    Initial Commit

---
+++ Travisty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travisty
+//
+//  Created by Michael Higgins on 12/26/17.
+//  Copyright © 2017 Michael Higgins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
